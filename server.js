57e56f3455a99bf723b4b354ac4c0692c2626fab
Petro <larrypetroski@gmail.com>
2021-10-18 21:21:10 -0500

    added compression

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
+const compression = require('compression');
 
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
